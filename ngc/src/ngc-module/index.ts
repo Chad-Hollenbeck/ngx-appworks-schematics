581a26d840066243e668c79650a9eb930763ae0d
Chad Hollenbeck <chad.hollenbeck@owlcomputing.com>
2019-12-10 16:36:46 -0500

    component registration complete.

--- ngc/src/ngc-module/index.ts
+++ ngc/src/ngc-module/index.ts
@@ -11,20 +11,20 @@ export function ngcModule(_options: any): Rule {
 
     const parsedPath = parseName(defaultProjectPath, _options.name);
 
-    const { path } = parsedPath;
-    // const modelsPath = path + '/models';
+    const {name, path } = parsedPath;
+    const componentPath = path + '/' + name;
 
     const sourceTemplates = url('./templates');
 
     // console.log(_context);
-    // _options.name = name.slice(1);
+    _options.name = name.slice(1);
     // console.log(_options.name);
 
     const sourceParametrized = apply(sourceTemplates, [
       template({
         ..._options,
         ...strings
-      }), move(path)
+      }), move(componentPath)
     ]);
 
     return mergeWith(sourceParametrized)(tree, _context);
